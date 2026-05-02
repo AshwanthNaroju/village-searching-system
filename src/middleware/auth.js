865d164b72b8db24fa60136de0daf01c94c0f802
Naroju Ashwanth <narojuashwanth877@gmail.com>
2026-05-02 15:59:05 +0530

    Create auth.js

---
+++ src/middleware/auth.js
@@ -0,0 +1,24 @@
+const jwt = require("jsonwebtoken");
+
+const verifyToken = (req, res, next) => {
+  const token = req.headers["authorization"];
+
+  if (!token) return res.status(401).json({ message: "No token" });
+
+  try {
+    const decoded = jwt.verify(token, "SECRET_KEY");
+    req.user = decoded;
+    next();
+  } catch {
+    res.status(401).json({ message: "Invalid token" });
+  }
+};
+
+const isAdmin = (req, res, next) => {
+  if (req.user.role !== "admin") {
+    return res.status(403).json({ message: "Admin only" });
+  }
+  next();
+};
+
+module.exports = { verifyToken, isAdmin };
